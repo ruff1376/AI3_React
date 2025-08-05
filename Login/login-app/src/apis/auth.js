aecf96a454f528916b43b648c392f029f35aa7b6
ruff1376 <ruff1376@gmail.com>
2025-08-05 11:08:02 +0900

    회원정보 수정, 회원탈퇴

--- Login/login-app/src/apis/auth.js
+++ Login/login-app/src/apis/auth.js
@@ -10,3 +10,9 @@ export const login = (username, password) => {
 
 // 회원 정보
 export const info = () => api.get(`/users/info`)
+
+// 회원 정보 수정
+export const update = (data) => api.put(`/users`, data)
+
+// 회원 탈퇴
+export const remove = (username) => api.delete(`/users/${username}`)
--- Login/login-app/src/components/User/UserForm.jsx
+++ Login/login-app/src/components/User/UserForm.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
 
-const UserForm = () => {
+const UserForm = ({ userInfo, updateUser, deleteUser }) => {
+
+    // 정보 수정
+    const onUpdate = (e) => {
+        e.preventDefault()
+
+        const form = e.target
+        const username = form.username.value
+        const password = form.password.value
+        const name = form.name.value
+        const email = form.email.value
+        console.log( username, password, name, email );
+
+        updateUser( { username, password, name, email } )
+    }
+
     return (
-        <div>UserForm</div>
+        <div className="form">
+            <h2 className="login-title">회원 정보</h2>
+            <form className="login-form" onSubmit={ (e) => onUpdate(e) }>
+                <div>
+                    <label htmlFor="username">username</label>
+                    <input type="text" name="username" id="username" placeholder='username' autoComplete='username' required readOnly defaultValue={ userInfo?.username } />
+                </div>
+                <div>
+                    <label htmlFor="password">password</label>
+                    <input type="password" name="password" id="password" placeholder='password' autoComplete='password' required />
+                </div>
+                <div>
+                    <label htmlFor="name">name</label>
+                    <input type="text" name="name" id="name" placeholder='name' autoComplete='name' required defaultValue={ userInfo?.name } />
+                </div>
+                <div>
+                    <label htmlFor="email">email</label>
+                    <input type="email" name="email" id="email" placeholder='email' autoComplete='email' required defaultValue={ userInfo?.email } />
+                </div>
+                <button type="submit" className="btn btn--form btn-login">정보 수정</button>
+                <button className="btn btn--form btn-login" onClick={ () => deleteUser(userInfo.username) }>회원 탈퇴</button>
+            </form>
+        </div>
     )
 }
 
